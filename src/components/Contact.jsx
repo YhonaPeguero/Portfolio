f1d06560cf102b498a1e700d475b04e1e3ac6001
YhonaPeguero <2017-0516@unad.edu.do>
2022-10-05 20:50:31 -0400

    refactoring contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,9 +7,10 @@ const Contact = () => {
             /* le quito el h-screen y le paso de p-4 a p-8 */
             className="w-full bg-[#0a192f] flex justify-center items-center p-8 mt-10"
         >
-            <form
+            <form name="contact" method="post"
                 className="flex flex-col max-w-[600px] w-full"
             >
+               
                 <div className="pb-8">
                     <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
                         Contáctame
@@ -18,7 +19,13 @@ const Contact = () => {
                     ¡Espero que podamos hablar pronto! enviame un mensaje para charlar sobre tu proyecto.
                     </p>
                 </div>
-                <input type="hidden" name="form-name" value="contact"
+
+                <input 
+                type="hidden" 
+                name="form-name" 
+                value="contact" 
+                />
+                <input
                     className="bg-[#ccd6f6] p-2"
                     placeholder="Nombre"
                 />
@@ -34,7 +41,7 @@ const Contact = () => {
                     rows="10"
                     placeholder="Mensaje"
                 ></textarea>
-                <button className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center">
+                <button type='submit' className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center">
                     Enviar Mensaje
                 </button>
             </form>
